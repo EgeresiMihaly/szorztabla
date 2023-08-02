9e285af0f1c95765163a5dc419df7aee08dc23c4
Linuxfun51 <91023363+EgeresiMihaly@users.noreply.github.com>
2023-08-02 16:26:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace szorztabla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            if (args.Length == 0)
+            {
+                Random random = new Random();
+                number = random.Next(100);
+                
+
+            }
+            else
+            {
+                number = int.Parse(args[0]);
+               
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", i, number, i * number);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
